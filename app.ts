cb80183c655fba98b8fa3edd4ba8d0bbf5dd5abf
zefirez@zoho.com <tsumirez@zoho.com>
2021-04-29 22:42:32 +0200

    Tuple Type.
    Stuff you need to know is in the app.ts file's code comments.

--- app.ts
+++ app.ts
@@ -1,17 +1,24 @@
-let kickass = {
+let kickass: {
+    name: string;
+    age: number;
+    hobbies: string[],
+    role: [number, string] //tuple property definition. Necessary as TS does not infer tuples, mistaking them for normal arrays.
+} = {
     name: 'Zef',
     age: 39,
-    hobbies: ['programming','League of Legends','Mentalism']
+    hobbies: ['programming','League of Legends','Mentalism'],
+    //here we initialize our tuple.
+    //Tuple is a fixed sized array, that may contain elements of different types, but in a predefined order.
+    //Here first element must be a number, second a string and the number of elements must be two.
+    //without tuple definition in line 5, all of that would not be enforced and TS would see an array of numbers or strings,
+    //where both values being just numbers, or number of elements being different than two would be allowed.
+    role: [1, 'precursor']
 }
 
 let fav_anime: string[]; //this means that anime type is an array containing only strings. 
-fav_anime = ['Slayers','Psycho Pass','Saber Marionette J']; //trap here! adding values like this is fine and dandy.
-// but if you try via fav_anime.push(), TS will let it through but js will have runtime error! In js it's not an array until 
-// you initialized it as such, and typescript array type declaration in line 7 does not count as such. For JS you just tried 
-// to use an array method on named, but not initialized variable, therefore making it confused and throwing error.
+fav_anime = ['Slayers','Psycho Pass','Saber Marionette J']; 
 
 
 for (let anime of fav_anime) {
-    console.log(anime.toUpperCase()); //typescript recognizes that each entry from array fav_anime is a string and has no 
-    //issues with using string methods on it
+    console.log(anime.toUpperCase());
 }
