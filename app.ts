8b8805bc2be59c0d71be6b0c904d70769a7b8900
zefirez@zoho.com <tsumirez@zoho.com>
2021-04-30 07:21:19 +0200

    "any" Type.

--- app.ts
+++ app.ts
@@ -1,10 +1,3 @@
-//here we have our first ts custom type - enum
-//it allows to define key=>value pairs that can be used in reverse.
-//by default values you pass are given numeric keys starting from 0.
-//so Role[0] equals 'ADMIN', while Role.ADMIN will return 0.
-//this can however be totally overriten with different key values, no necessairly numbers at all!
-//that being said, non-numeric values will not allow for the reverse value=>key assignement.
-//Role.REVIEWER will return 'recenzent', but ROLE.recenzent will return undefined.
 enum Role {ADMIN=1,USER=100,REVIEWER='recenzent'}
 
 let kickass = {
@@ -14,7 +7,15 @@ let kickass = {
     role: Role.REVIEWER
 }
 
-let fav_anime: string[]; //this means that anime type is an array containing only strings. 
+//any type is just what it says - allowance to use any type or comination of.
+//in short TS will let anything slide and not bother. Obviously this is contradictory
+//to the purpose of TS - which is enforcing types and stricter control.
+//use this type only when you just can't know what will be stored in given variable/property, yet 
+//you need it to function. It's a "back off pal, I got this" command to TS.
+let challenge: any;
+challenge = ['wash dishes', 'exercise', 'groceries', {languages: ['hindi']}];
+
+let fav_anime: string[];
 fav_anime = ['Slayers','Psycho Pass','Saber Marionette J']; 
 
 
