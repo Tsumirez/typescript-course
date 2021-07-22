8dca4074a85e083e2bcbf13e208534803a71b8c6
zefirez@zoho.com <tsumirez@zoho.com>
2021-07-22 14:06:40 +0200

    Source maps - explanation in app.ts file

--- analitics.js
+++ analitics.js
@@ -1,2 +1,3 @@
 "use strict";
 console.log('analizing..and gone!');
+//# sourceMappingURL=analitics.js.map
--- app.ts
+++ app.ts
@@ -1,17 +1,12 @@
 /*
-Additional config options part 1:
+Source maps.
 
-    // "allowJs": true,                             /* Allow javascript files to be compiled. */
-    // "checkJs": true,                             /* Report errors in .js files. */
-    // "jsx": "preserve",                           /* Specify JSX code generation: 'preserve', 'react-native', 'react', 'react-jsx' or 'react-jsxdev'. */
-    // "declaration": true,                         /* Generates corresponding '.d.ts' file. */
-    // "declarationMap": true,                      /* Generates a sourcemap for each corresponding '.d.ts' file. */
-/*
-  First two make ts compiler check out js files (normally it only cares about ts ones). Can be useful to transpile into other js version
-  or getting a free-check-up on them for some mistakes.
+    // "sourceMap": true,                      Generates corresponding '.map' file.
+    
+Set this to true and sourcemaps will be created for compiled .ts files
+These allow browser's devtools (least chrome ones) to access typescript files and debug them.
+Normally these would not be accessible to the browser, only their end result .js files.
 
-  jsx - plain and simple - if it's a react project you'll want to fiddle with this option.
-  declaration and declartions maps are for typscript projects bundled as libraries - not in our scope of interest atm.
 */
 
 console.log('read the comment in file');
