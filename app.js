6a37b46a3848f522ef2af401a933c456c29c03a4
zefirez@zoho.com <tsumirez@zoho.com>
2021-07-22 13:19:25 +0200

    Ts core libs - info in ts.app file

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 "use strict";
-//TS compiler Watch Mode
-// tsc -w app.ts
-//simple as that. Add -w or --watch flag and typescript compiler will behave much the same as lite server,
-//automatically compiling and updating output js file when you save a typescript source one.
-console.log('This one is about typscript compiler\'s watch mode. Details in the app.ts file');
+/*
+TS core libs.
+
+You'll notice that tsconfig.jsob as a commented "lib" entry.
+This entry specifies libs needed for ts to work properly.
+Having it commented  out means including 4 default ones:
+"dom", "es6", "dom.iterable" and "scripthost".
+
+These are needed for ts' basic functionality - recognizing js dom calls, methods (like console.log)
+es6 syntax support and so on.
+
+You can check this to be true by uncommenting the lib array and not passing it any value.
+At that point ts will fail to recognize the most basic js objects or document object model methods and objects for example.
+*/
+console.log('');
--- app.ts
+++ app.ts
@@ -1,8 +1,16 @@
-//TS compiler Watch Mode
+/*
+TS core libs. 
 
-// tsc -w app.ts
+You'll notice that tsconfig.jsob as a commented "lib" entry.
+This entry specifies libs needed for ts to work properly.
+Having it commented  out means including 4 default ones:
+"dom", "es6", "dom.iterable" and "scripthost".
 
-//simple as that. Add -w or --watch flag and typescript compiler will behave much the same as lite server,
-//automatically compiling and updating output js file when you save a typescript source one.
+These are needed for ts' basic functionality - recognizing js dom calls, methods (like console.log)
+es6 syntax support and so on. 
 
-console.log('This one is about typscript compiler\'s watch mode. Details in the app.ts file');
+You can check this to be true by uncommenting the lib array and not passing it any value.
+At that point ts will fail to recognize the most basic js objects or document object model methods and objects for example.
+*/
+
+console.log('');
