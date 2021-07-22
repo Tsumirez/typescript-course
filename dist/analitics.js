79e8991a96b59f82d4aab37a8e7d010a30f002b0
zefirez@zoho.com <tsumirez@zoho.com>
2021-07-22 15:52:39 +0200

    noEmitOnError setting.

--- dist/analitics.js
+++ dist/analitics.js
@@ -1,2 +1,5 @@
 "use strict";
 console.log('analizing..and gone!');
+let input = document.querySelector('#input');
+let z;
+z = 5 + parseInt(input.value);
--- src/analitics.ts
+++ src/analitics.ts
@@ -1,1 +1,7 @@
 console.log('analizing..and gone!');
+
+let input = document.querySelector('#input')! as HTMLInputElement;
+
+let z:number; // comment 
+
+z = 5 + parseInt(input.value);
